fe0ea399f9411a8e523c3124615725431a93d593
Ike Ton <Ike Ton>
2012-08-26 13:05:19 -0500

    move data into classes.
    Move all the data for players and bullets into classes.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,24 @@ using System.Text;
 
 namespace Lesson3
 {
+    class Player
+    {
+        // Screen coord. Here we store where we want the player to be.
+        // [0,0] [0,1] [0,2] [0,3] ...
+        // [1,0] [1,1] [1,2] [1,3] ...
+        public int m_xPos = 20;
+        public int m_yPos = 10;
+
+        // when the player dies, we want to him to stay dead for so many frames. if this value is greater then 1 then he is dead.
+        public int m_deathTime = 0;
+    }
+
+    class Bullet
+    {
+        public int m_xPos = 20;
+        public int m_yPos = 10;
+    }
+
     class Program
     {
         // wheither or not exit
@@ -13,27 +31,17 @@ namespace Lesson3
         // we will want to add some randomness to our game so we need a random number generator.
         static Random s_RandomNumberGenerator = new Random();
 
-        // Screen coord. Here we store where we want the player to be.
-        // [0,0] [0,1] [0,2] [0,3] ...
-        // [1,0] [1,1] [1,2] [1,3] ...
-        static int s_PlayerXPos = 20;
-        static int s_PlayerYPos = 10;
-
-        // when the player dies, we want to him to stay dead for so many frames. if this value is greater then 1 then he is dead.
-        static int s_PlayerDeathTime = 0;
-
-        // Screen coord. Here we are going to want to store the position of the bullet.
-        // we want this to be an array of 5. So we can have different bullets running at the same time.
-        static int[] s_bulletXPos = new int[5];
-        static int[] s_bulletYPos = new int[5];
+        static Player s_player = new Player();
+        static Bullet[] s_bullet = new Bullet[5];
 
         static void Main(string[] args)
         {
             // we now need to initialize all the bullets
             for (int i = 0; i < 5; ++i)
             {
-                s_bulletXPos[i] = s_RandomNumberGenerator.Next(40);
-                s_bulletYPos[i] = s_RandomNumberGenerator.Next(20);
+                s_bullet[i] = new Bullet();
+                s_bullet[i].m_xPos = s_RandomNumberGenerator.Next(40);
+                s_bullet[i].m_yPos = s_RandomNumberGenerator.Next(20);
             }
 
             // this is the game loop... every frame until the game exits
@@ -52,10 +60,10 @@ namespace Lesson3
 
         static void UpdateGameLogic()
         {
-            if (s_PlayerDeathTime > 0)
+            if (s_player.m_deathTime > 0)
             {
                 // if we are dead do not allow us to do anything
-                s_PlayerDeathTime--;
+                s_player.m_deathTime--;
             }
             else
             {
@@ -72,30 +80,30 @@ namespace Lesson3
                     // move the player based on the arrow keys pressed
                     if (key.Key == ConsoleKey.DownArrow)
                     {
-                        s_PlayerYPos += 1;
+                        s_player.m_yPos += 1;
                     }
                     if (key.Key == ConsoleKey.UpArrow)
                     {
-                        s_PlayerYPos -= 1;
+                        s_player.m_yPos -= 1;
                     }
                     if (key.Key == ConsoleKey.RightArrow)
                     {
-                        s_PlayerXPos += 1;
+                        s_player.m_xPos += 1;
                     }
                     if (key.Key == ConsoleKey.LeftArrow)
                     {
-                        s_PlayerXPos -= 1;
+                        s_player.m_xPos -= 1;
                     }
 
                     // make sure the player position does not go off the screen
-                    if (s_PlayerXPos < 0)
-                        s_PlayerXPos = 0;
-                    if (s_PlayerYPos < 0)
-                        s_PlayerYPos = 0;
-                    if (s_PlayerXPos > 40)
-                        s_PlayerXPos = 40;
-                    if (s_PlayerYPos > 20)
-                        s_PlayerYPos = 20;
+                    if (s_player.m_xPos < 0)
+                        s_player.m_xPos = 0;
+                    if (s_player.m_yPos < 0)
+                        s_player.m_yPos = 0;
+                    if (s_player.m_xPos > 40)
+                        s_player.m_xPos = 40;
+                    if (s_player.m_yPos > 20)
+                        s_player.m_yPos = 20;
                 }
             }
 
@@ -103,27 +111,27 @@ namespace Lesson3
             for (int i=0; i<5; ++i)
             {
                 // now we are going to update the bullets movement
-                s_bulletXPos[i] -= 1;
+                s_bullet[i].m_xPos -= 1;
 
                 // if the bullet hits the end we want it to wrap around
-                if (s_bulletXPos[i] < 0)
+                if (s_bullet[i].m_xPos < 0)
                 {
-                    s_bulletXPos[i] = 40;
-                    s_bulletYPos[i] = s_RandomNumberGenerator.Next(20);
+                    s_bullet[i].m_xPos = 40;
+                    s_bullet[i].m_yPos = s_RandomNumberGenerator.Next(20);
                 }
 
                 // now we can check if the bullet hit the player
-                if (s_bulletXPos[i] == s_PlayerXPos
-                    && s_bulletYPos[i] == s_PlayerYPos)
+                if (s_bullet[i].m_xPos == s_player.m_xPos
+                    && s_bullet[i].m_yPos == s_player.m_yPos)
                 { 
                     // there is a hit if they occupy the same space!
                     // we will handle this in the next exercise.
 
-                    s_PlayerDeathTime = 20;
+                    s_player.m_deathTime = 20;
 
                     // move the bullet back
-                    s_bulletXPos[i] = 40;
-                    s_bulletYPos[i] = s_RandomNumberGenerator.Next(20);
+                    s_bullet[i].m_xPos = 40;
+                    s_bullet[i].m_yPos = s_RandomNumberGenerator.Next(20);
                 }
             }
         }
@@ -134,10 +142,10 @@ namespace Lesson3
             Console.Clear();
 
             // move the cursor to the screen position 
-            Console.SetCursorPosition(s_PlayerXPos, s_PlayerYPos);
+            Console.SetCursorPosition(s_player.m_xPos, s_player.m_yPos);
             
             // Draw the player
-            if (s_PlayerDeathTime > 0)
+            if (s_player.m_deathTime > 0)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write("X"); // X if he is dead
@@ -152,7 +160,7 @@ namespace Lesson3
             for (int i = 0; i < 5; ++i)
             {
                 // now we need to draw the bullet
-                Console.SetCursorPosition(s_bulletXPos[i], s_bulletYPos[i]);
+                Console.SetCursorPosition(s_bullet[i].m_xPos, s_bullet[i].m_yPos);
                 Console.Write(".");
             }
         }
--- readme.txt
+++ readme.txt
@@ -4,3 +4,4 @@ The main purpose of this lesson is to introduce classes to organize code.
 
 - Check in #1. We copy the lesson 2
 
+- Check in #2. Move all the data for players and bullets into classes.
