6428c5198745a6ef36738de70fc9d7d7e238a555
Ike Ton <Ike Ton>
2012-08-26 13:26:36 -0500

    put logic into the classes
    Move all the logic into the player and bullet classes

--- Program.cs
+++ Program.cs
@@ -15,29 +15,130 @@ namespace Lesson3
 
         // when the player dies, we want to him to stay dead for so many frames. if this value is greater then 1 then he is dead.
         public int m_deathTime = 0;
+
+        public void Update()
+        {
+            if (m_deathTime > 0)
+            {
+                // if we are dead do not allow us to do anything
+                m_deathTime--;
+            }
+            else
+            {
+                // we need to check if the user has pressed any keys
+                if (Console.KeyAvailable)
+                {
+                    // get the key that was pressed, if the key was escape
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Escape)
+                    {
+                        Program.s_ExitGame = true;
+                    }
+
+                    // move the player based on the arrow keys pressed
+                    if (key.Key == ConsoleKey.DownArrow)
+                    {
+                        m_yPos += 1;
+                    }
+                    if (key.Key == ConsoleKey.UpArrow)
+                    {
+                        m_yPos -= 1;
+                    }
+                    if (key.Key == ConsoleKey.RightArrow)
+                    {
+                        m_xPos += 1;
+                    }
+                    if (key.Key == ConsoleKey.LeftArrow)
+                    {
+                        m_xPos -= 1;
+                    }
+
+                    // make sure the player position does not go off the screen
+                    if (m_xPos < 0)
+                        m_xPos = 0;
+                    if (m_yPos < 0)
+                        m_yPos = 0;
+                    if (m_xPos > 40)
+                        m_xPos = 40;
+                    if (m_yPos > 20)
+                        m_yPos = 20;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            // move the cursor to the screen position 
+            Console.SetCursorPosition(m_xPos, m_yPos);
+            
+            if (m_deathTime > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("X"); // X if he is dead
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.Write("O"); // O if he is alive
+            }
+        }
     }
 
     class Bullet
     {
         public int m_xPos = 20;
         public int m_yPos = 10;
+
+        public void Update()
+        {
+            // now we are going to update the bullets movement
+            m_xPos -= 1;
+
+            // if the bullet hits the end we want it to wrap around
+            if (m_xPos < 0)
+            {
+                m_xPos = 40;
+                m_yPos = Program.s_RandomNumberGenerator.Next(20);
+            }
+
+            // now we can check if the bullet hit the player
+            if (m_xPos == Program.s_player.m_xPos
+                && m_yPos == Program.s_player.m_yPos)
+            {
+                // there is a hit if they occupy the same space!
+                // we will handle this in the next exercise.
+
+                Program.s_player.m_deathTime = 20;
+
+                // move the bullet back
+                m_xPos = 40;
+                m_yPos = Program.s_RandomNumberGenerator.Next(20);
+            }
+        }
+
+        public void Draw()
+        {                
+            // now we need to draw the bullet
+            Console.SetCursorPosition(m_xPos, m_yPos);
+            Console.Write("."); 
+        }
     }
 
     class Program
     {
         // wheither or not exit
-        static bool s_ExitGame = false;
+        public static bool s_ExitGame = false;
 
         // we will want to add some randomness to our game so we need a random number generator.
-        static Random s_RandomNumberGenerator = new Random();
+        public static Random s_RandomNumberGenerator = new Random();
 
-        static Player s_player = new Player();
+        public static Player s_player = new Player();
         static Bullet[] s_bullet = new Bullet[5];
 
         static void Main(string[] args)
         {
             // we now need to initialize all the bullets
-            for (int i = 0; i < 5; ++i)
+            for (int i = 0; i < s_bullet.Length; ++i)
             {
                 s_bullet[i] = new Bullet();
                 s_bullet[i].m_xPos = s_RandomNumberGenerator.Next(40);
@@ -60,79 +161,12 @@ namespace Lesson3
 
         static void UpdateGameLogic()
         {
-            if (s_player.m_deathTime > 0)
-            {
-                // if we are dead do not allow us to do anything
-                s_player.m_deathTime--;
-            }
-            else
-            {
-                // we need to check if the user has pressed any keys
-                if (Console.KeyAvailable)
-                {
-                    // get the key that was pressed, if the key was escape
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    if (key.Key == ConsoleKey.Escape)
-                    {
-                        s_ExitGame = true;
-                    }
-
-                    // move the player based on the arrow keys pressed
-                    if (key.Key == ConsoleKey.DownArrow)
-                    {
-                        s_player.m_yPos += 1;
-                    }
-                    if (key.Key == ConsoleKey.UpArrow)
-                    {
-                        s_player.m_yPos -= 1;
-                    }
-                    if (key.Key == ConsoleKey.RightArrow)
-                    {
-                        s_player.m_xPos += 1;
-                    }
-                    if (key.Key == ConsoleKey.LeftArrow)
-                    {
-                        s_player.m_xPos -= 1;
-                    }
-
-                    // make sure the player position does not go off the screen
-                    if (s_player.m_xPos < 0)
-                        s_player.m_xPos = 0;
-                    if (s_player.m_yPos < 0)
-                        s_player.m_yPos = 0;
-                    if (s_player.m_xPos > 40)
-                        s_player.m_xPos = 40;
-                    if (s_player.m_yPos > 20)
-                        s_player.m_yPos = 20;
-                }
-            }
+            s_player.Update();
 
             // instead of update the 1 bullet we update each of the 5
-            for (int i=0; i<5; ++i)
+            for (int i = 0; i < s_bullet.Length; ++i)
             {
-                // now we are going to update the bullets movement
-                s_bullet[i].m_xPos -= 1;
-
-                // if the bullet hits the end we want it to wrap around
-                if (s_bullet[i].m_xPos < 0)
-                {
-                    s_bullet[i].m_xPos = 40;
-                    s_bullet[i].m_yPos = s_RandomNumberGenerator.Next(20);
-                }
-
-                // now we can check if the bullet hit the player
-                if (s_bullet[i].m_xPos == s_player.m_xPos
-                    && s_bullet[i].m_yPos == s_player.m_yPos)
-                { 
-                    // there is a hit if they occupy the same space!
-                    // we will handle this in the next exercise.
-
-                    s_player.m_deathTime = 20;
-
-                    // move the bullet back
-                    s_bullet[i].m_xPos = 40;
-                    s_bullet[i].m_yPos = s_RandomNumberGenerator.Next(20);
-                }
+                s_bullet[i].Update();
             }
         }
 
@@ -141,27 +175,13 @@ namespace Lesson3
             // clear the screen
             Console.Clear();
 
-            // move the cursor to the screen position 
-            Console.SetCursorPosition(s_player.m_xPos, s_player.m_yPos);
-            
             // Draw the player
-            if (s_player.m_deathTime > 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("X"); // X if he is dead
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else
-            {
-                Console.Write("O"); // O if he is alive
-            }
+            s_player.Draw();
 
             // instead of drawing the 1 bullet we update each of the 5
-            for (int i = 0; i < 5; ++i)
+            for (int i = 0; i < s_bullet.Length; ++i)
             {
-                // now we need to draw the bullet
-                Console.SetCursorPosition(s_bullet[i].m_xPos, s_bullet[i].m_yPos);
-                Console.Write(".");
+                s_bullet[i].Draw();
             }
         }
     }
--- readme.txt
+++ readme.txt
@@ -5,3 +5,5 @@ The main purpose of this lesson is to introduce classes to organize code.
 - Check in #1. We copy the lesson 2
 
 - Check in #2. Move all the data for players and bullets into classes.
+
+- Check in #3. Move all the logic into the player and bullet classes.
